a9f2956bf3feaf04409d95fcb5b08152c554fd46
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2024-06-18 20:52:05 -0400

    add ball and net

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+const express = require('express');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname))
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log('server running at port 3000');
+});
